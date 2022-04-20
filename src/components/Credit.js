0c11d9a887aa47a63346804c36640ecbdad0c948
unknown <kalantarlikamran2@gmail.com>
2022-04-20 06:07:21 +0400

    zeynebapp

---
+++ src/components/Credit.js
@@ -0,0 +1,34 @@
+import React from "react";
+import styled from "styled-components";
+const Credit = () => {
+	return (
+		<CreditContainer>
+			<Link href="https://www.intagram.com" target="_blank">
+				Zeyneb Aliyeva
+			</Link>
+			<br></br>
+			<Link href="https://github.com/" target="_blank">
+				Spotify
+			</Link>
+		</CreditContainer>
+	);
+};
+
+const CreditContainer = styled.div`
+	user-select: none;
+	position: fixed;
+	display: flex;
+	flex-direction: column;
+	align-items: flex-end;
+	justify-content: flex-end;
+	z-index: 12;
+	bottom: 5px;
+	right: 5px;
+	color: rgb(155, 155, 155);
+	font-size: 0.75rem;
+`;
+
+const Link = styled.a`
+	color: rgb(155, 155, 155);
+`;
+export default Credit;
---
+++ src/data.js
@@ -0,0 +1,68 @@
+import { v4 as uuidv4 } from "uuid";
+function chillHop() {
+	return [
+		{
+			name: "Beaver Creek",
+			cover:
+				"https://niezlasztuka.net/wp-content/uploads/2019/02/Zimna-wojna-6.jpg",
+			artist: "Aso, Middle School, Aviino",
+			audio: "https://soundcloud.com/subwax-bcn-distribution/daudio001?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing",
+			color: ["#205950", "#2ab3bf"],
+			id: uuidv4(),
+			active: true,
+		},
+		{
+			name: "Daylight",
+			cover:
+				"https://niezlasztuka.net/wp-content/uploads/2019/02/zimna-wojna-8.jpg?v=1575462754",
+			artist: "Aiguille",
+			audio: "https://mp3.chillhop.com/serve.php/?mp3=9272",
+			color: ["#EF8EA9", "#ab417f"],
+			id: uuidv4(),
+			active: false,
+		},
+		{
+			name: "Keep Going",
+			cover:
+				"https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg",
+			artist: "Swørn",
+			audio: "https://mp3.chillhop.com/serve.php/?mp3=9222",
+			color: ["#CD607D", "#c94043"],
+			id: uuidv4(),
+			active: false,
+		},
+		{
+			name: "Nightfall",
+			cover:
+				"https://chillhop.com/wp-content/uploads/2020/07/ef95e219a44869318b7806e9f0f794a1f9c451e4-1024x1024.jpg",
+			artist: "Aiguille",
+			audio: "https://ve36.aadika.xyz/download/mwgZalAFNhM/mp3/128/1650362272/e2f9de76f1c8e30f1c32908cb0f5e3f6b10115a806a76d2a9394b47114a57b2e/1?f=X2Download.com",
+			color: ["#EF8EA9", "#ab417f"],
+			id: uuidv4(),
+			active: false,
+		},
+		{
+			name: "Reflection",
+			cover:
+				"https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg",
+			artist: "Swørn",
+			audio: "https://ve36.aadika.xyz/download/mwgZalAFNhM/mp3/128/1650362272/e2f9de76f1c8e30f1c32908cb0f5e3f6b10115a806a76d2a9394b47114a57b2e/1?f=X2Download.com",
+			color: ["#CD607D", "#c94043"],
+			id: uuidv4(),
+			active: false,
+		},
+		{
+			name: "Under the City Stars",
+			cover:
+				"https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
+			artist: "Aso, Middle School, Aviino",
+			audio: "https://mp3.chillhop.com/serve.php/?mp3=10074",
+			color: ["#205950", "#2ab3bf"],
+			id: uuidv4(),
+			active: false,
+		},
+		//ADD MORE HERE
+	];
+}
+
+export default chillHop;
